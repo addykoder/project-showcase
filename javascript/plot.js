f9455c209f4f7e70d96485429fd2fb46d3a8e1ec
Aditya Tripathi <productive.py@gmail.com>
2022-06-14 23:22:09 +0530

    almost finalised

--- javascript/plot.js
+++ javascript/plot.js
@@ -1,143 +1,12 @@
 let projects = [
+
 	{
 		name: 'Project Showcase',
 		description: 'Personal Website that displays the projects made by me, probably where you are reading this text.',
 		technologies: ['html', 'css', 'javascript'],
 		source: 'https://github.com/AddyKoder/project-showcase',
 		link: 'https://addykoder.github.io/project-showcase/',
-		image: 'project.jpg',
-	},
-
-	{
-		name: 'Blog Page',
-		description: 'Modern blog page with easy and readable styled content',
-		technologies: ['html', 'css', 'javascript'],
-
-		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/Modern%20Blog',
-		link: 'https://addykoder.github.io/DemoWebsite/Modern%20Blog/blog.html',
-
-		image: 'project.jpg',
-	},
-
-	{
-		name: 'Addy Cart',
-		description: 'Simple page with Ecommerce product cards and a sliding cart menu',
-		technologies: ['html', 'css', 'javascript'],
-
-		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/AddyCart',
-		link: 'https://addykoder.github.io/DemoWebsite/AddyCart/addycart.html',
-
-		image: 'project.jpg',
-	},
-
-	{
-		name: 'Zomato Clone',
-		description: 'This was a project that I started but never finished. LOL',
-		technologies: ['html', 'css'],
-
-		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/zomato%20clone',
-		link: 'https://addykoder.github.io/DemoWebsite/zomato%20clone/index.html',
-
-		image: 'project.jpg',
-	},
-
-	{
-		name: 'Notes App',
-		description: 'Single Page web app that uses your local storage to store notes created by users',
-		technologies: ['html', 'css', 'javascript'],
-
-		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/Notes%20App',
-		link: 'https://addykoder.github.io/DemoWebsite/Notes%20App/index.html',
-
-		image: 'project.jpg',
-	},
-
-	{
-		name: 'PUBG Form',
-		description: 'The first usable page that I designed using html and css',
-		technologies: ['html', 'css'],
-
-		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/project1',
-		link: 'https://addykoder.github.io/DemoWebsite/project1/html/page1.html',
-
-		image: 'project.jpg',
-	},
-
-	{
-		name: 'Navigation Bars',
-		description: 'This page contains a set of modern navigation bars with cool hover effects',
-		technologies: ['html', 'css'],
-
-		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/project2',
-		link: 'https://addykoder.github.io/DemoWebsite/project2/html/page2.html',
-
-		image: 'project.jpg',
-	},
-
-	{
-		name: 'Swiggy Clone',
-		description: 'The cloned page of official swiggy desktop web page. Not finished due to lack of motivation.',
-		technologies: ['html', 'css'],
-
-		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/project3',
-		link: 'https://addykoder.github.io/DemoWebsite/project3/index.html',
-
-		image: 'project.jpg',
-	},
-
-	{
-		name: 'Old Blog Page',
-		description: 'Simple Blog page template which I created way back then when I was learning to design',
-		technologies: ['html', 'css'],
-
-		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/project4',
-		link: 'https://addykoder.github.io/DemoWebsite/project4/page4.html',
-
-		image: 'project.jpg',
-	},
-
-	{
-		name: 'Posty',
-		description: 'A Full-Stack Web app in which the user can create and modify thier post and also see their feeds of other users post',
-		technologies: ['html', 'css', 'javascript', 'node', 'mongodb'],
-
-		source: 'https://github.com/AddyKoder/Posty',
-		link: 'https://posty-addykoder.herokuapp.com/',
-
-		image: 'project.jpg',
-	},
-
-	{
-		name: 'Reque Star',
-		description: 'A web app that helps you check your backend by making http requests, similar(Copy) of postman',
-		technologies: ['html', 'css', 'javascript'],
-
-		source: 'https://github.com/AddyKoder/P4---RequeStar',
-		link: 'https://addykoder.github.io/P4---RequeStar/',
-
-		image: 'project.jpg',
-	},
-
-	{
-		name: 'Library Manager',
-		description: 'A web app that provides easy interface to manage books in a library. It has varies operations for books.',
-		technologies: ['html', 'css', 'javascript'],
-
-		source: 'https://github.com/AddyKoder/Library-Manager',
-		link: 'https://addykoder.github.io/Library-Manager/',
-
-		image: 'project.jpg',
-	},
-
-	{
-		name: 'Form Validation',
-		description: 'A very Simple Form interface that uses javascript regular expressions to validate user input',
-		technologies: ['html', 'css', 'javascript'],
-
-		source: 'https://github.com/AddyKoder/Javascript-Projects/tree/master/P3%20-%20Form%20Validation',
-		link: 'https://addykoder.github.io/Javascript-Projects/P3%20-%20Form%20Validation/',
-
-		image: 'project.jpg',
+		image: 'showcase.jpg',
 	},
 
 	{
@@ -148,7 +17,20 @@ let projects = [
 		source: 'https://github.com/AddyKoder/AdityaTripathi',
 		link: 'https://adityatripathi.com',
 
-		image: 'project.jpg',
+		image: 'personal.jpg',
+	},
+
+
+
+	{
+		name: 'Blog Page',
+		description: 'Modern blog page with easy and readable styled content',
+		technologies: ['html', 'css', 'javascript'],
+
+		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/Modern%20Blog',
+		link: 'https://addykoder.github.io/DemoWebsite/Modern%20Blog/blog.html',
+
+		image: 'blog.jpg',
 	},
 
 	{
@@ -159,8 +41,149 @@ let projects = [
 		source: 'https://github.com/AddyKoder/MagicNotes-Express',
 		link: 'https://magicnotes-express.herokuapp.com/',
 
-		image: 'project.jpg',
+		image: 'magicnotes.jpg',
 	},
+
+	{
+		name: 'Posty',
+		description: 'A Full-Stack Web app in which the user can create and modify thier post and also see their feeds of other users post',
+		technologies: ['html', 'css', 'javascript', 'node', 'mongodb'],
+
+		source: 'https://github.com/AddyKoder/Posty',
+		link: 'https://posty-addykoder.herokuapp.com/',
+
+		image: 'posty.jpg',
+	},
+
+
+
+	{
+		name: 'Zomato Clone',
+		description: 'This was a project that I started but never finished. LOL',
+		technologies: ['html', 'css'],
+
+		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/zomato%20clone',
+		link: 'https://addykoder.github.io/DemoWebsite/zomato%20clone/index.html',
+
+		image: 'zomato.jpg',
+	},
+
+	{
+		name: 'Addy Cart',
+		description: 'Simple page with Ecommerce product cards and a sliding cart menu',
+		technologies: ['html', 'css', 'javascript'],
+
+		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/AddyCart',
+		link: 'https://addykoder.github.io/DemoWebsite/AddyCart/addycart.html',
+
+		image: 'addycart.jpg',
+	},
+
+
+	
+
+	
+
+	{
+		name: 'Library Manager',
+		description: 'A web app that provides easy interface to manage books in a library. It has varies operations for books.',
+		technologies: ['html', 'css', 'javascript'],
+
+		source: 'https://github.com/AddyKoder/Library-Manager',
+		link: 'https://addykoder.github.io/Library-Manager/',
+
+		image: 'library.jpg',
+	},
+
+	{
+		name: 'Reque Star',
+		description: 'A web app that helps you check your backend by making http requests, similar(Copy) of postman',
+		technologies: ['html', 'css', 'javascript'],
+
+		source: 'https://github.com/AddyKoder/P4---RequeStar',
+		link: 'https://addykoder.github.io/P4---RequeStar/',
+
+		image: 'requestar.jpg',
+	},
+
+	{
+		name: 'Swiggy Clone',
+		description: 'The cloned page of official swiggy desktop web page. Not finished due to lack of motivation.',
+		technologies: ['html', 'css'],
+
+		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/project3',
+		link: 'https://addykoder.github.io/DemoWebsite/project3/index.html',
+
+		image: 'swiggy.jpg',
+	},
+
+	{
+		name: 'Notes App',
+		description: 'Single Page web app that uses your local storage to store notes created by users',
+		technologies: ['html', 'css', 'javascript'],
+
+		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/Notes%20App',
+		link: 'https://addykoder.github.io/DemoWebsite/Notes%20App/index.html',
+
+		image: 'notes.jpg',
+	},
+
+	
+
+	{
+		name: 'Navigation Bars',
+		description: 'This page contains a set of modern navigation bars with cool hover effects',
+		technologies: ['html', 'css'],
+
+		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/project2',
+		link: 'https://addykoder.github.io/DemoWebsite/project2/html/page2.html',
+
+		image: 'nav.jpg',
+	},
+
+
+
+	{
+		name: 'Old Blog Page',
+		description: 'Simple Blog page template which I created way back then when I was learning to design',
+		technologies: ['html', 'css'],
+
+		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/project4',
+		link: 'https://addykoder.github.io/DemoWebsite/project4/page4.html',
+
+		image: 'oldblog.jpg',
+	},
+
+	{
+		name: 'PUBG Form',
+		description: 'The first usable page that I designed using html and css',
+		technologies: ['html', 'css'],
+
+		source: 'https://github.com/AddyKoder/DemoWebsite/tree/main/project1',
+		link: 'https://addykoder.github.io/DemoWebsite/project1/html/page1.html',
+
+		image: 'pubg.jpg',
+	},
+	
+
+	
+
+	
+
+	{
+		name: 'Form Validation',
+		description: 'A very Simple Form interface that uses javascript regular expressions to validate user input',
+		technologies: ['html', 'css', 'javascript'],
+
+		source: 'https://github.com/AddyKoder/Javascript-Projects/tree/master/P3%20-%20Form%20Validation',
+		link: 'https://addykoder.github.io/Javascript-Projects/P3%20-%20Form%20Validation/',
+
+		image: 'form.jpg',
+	},
+
+	
+
+	// ----------------------------------------------------------------
 
 	{
 		name: 'Tic Tac Toe',
